6f9dd0d8ae46bb409ecffd2ffaca7a4e4e915b47
Sean Chavez <realseanchavez@gmail.com>
2018-12-31 01:08:40 -0800

    riffle - iterative

--- riffle.js
+++ riffle.js
@@ -13,44 +13,62 @@
 //   }
 // }
 
-function isSingleRiffle(
-  half1,
-  half2,
-  shuffledDeck,
-  shuffledDeckIndex,
-  half1Index,
-  half2Index,
-) {
-  shuffledDeckIndex =
-    typeof shuffledDeckIndex !== 'undefined' ? shuffledDeckIndex : 0;
-  half1Index = typeof half1Index !== 'undefined' ? half1Index : 0;
-  half2Index = typeof half2Index !== 'undefined' ? half2Index : 0;
+// function isSingleRiffle(
+//   half1,
+//   half2,
+//   shuffledDeck,
+//   shuffledDeckIndex,
+//   half1Index,
+//   half2Index,
+// ) {
+//   shuffledDeckIndex =
+//     typeof shuffledDeckIndex !== 'undefined' ? shuffledDeckIndex : 0;
+//   half1Index = typeof half1Index !== 'undefined' ? half1Index : 0;
+//   half2Index = typeof half2Index !== 'undefined' ? half2Index : 0;
 
-  if (shuffleDeckIndex === shuffleDeck.length) {
-    return true;
+//   if (shuffleDeckIndex === shuffleDeck.length) {
+//     return true;
+//   }
+
+//   if (
+//     half1Index < half1.length &&
+//     half1[half1Index] === shuffledDeck[shuffledDeckIndex]
+//   ) {
+//     half1Index++;
+//   } else if (
+//     half2index < half2.length &&
+//     half2[half2Index] === shuffledDeck[shuffledDeckIndex]
+//   ) {
+//     half2Index++;
+//   } else {
+//     return false;
+//   }
+
+//   shuffledDeckIndex++;
+//   return isSingleRiffle(
+//     half1,
+//     half2,
+//     shuffleDeck,
+//     shuffledDeckIndex,
+//     half1Index,
+//     half2Index,
+//   );
+// }
+
+function isSingleRiffle(half1, half2, shuffleDeck) {
+  let half1Index = 0;
+  let half2Index = 0;
+
+  for (i = 0; i < shuffleDeck.length; i++) {
+    let card = shuffledDeck[i];
+
+    if (half1Index < half1.length && card === half1[half1Index]) {
+      half1Index++;
+    } else if (half2Index < half2.length && card === half2[half2Index]) {
+      half2Index++;
+    } else {
+      return false;
+    }
   }
-
-  if (
-    half1Index < half1.length &&
-    half1[half1Index] === shuffledDeck[shuffledDeckIndex]
-  ) {
-    half1Index++;
-  } else if (
-    half2index < half2.length &&
-    half2[half2Index] === shuffledDeck[shuffledDeckIndex]
-  ) {
-    half2Index++;
-  } else {
-    return false;
-  }
-
-  shuffledDeckIndex++;
-  return isSingleRiffle(
-    half1,
-    half2,
-    shuffleDeck,
-    shuffledDeckIndex,
-    half1Index,
-    half2Index,
-  );
+  return true;
 }
