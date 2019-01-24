fdd676dd929f0c9e63c017626d4d61813ec2edb0
Sean Chavez <realseanchavez@gmail.com>
2019-01-23 23:29:29 -0800

    first pass

--- mergeArrays.js
+++ mergeArrays.js
@@ -1,13 +1,32 @@
 function mergeArrays(myArray, yourArray) {
   const mergedArray = [];
-  const headOfMyArray = myArray[0];
-  const headOfYourArray = yourArray[0];
 
-  if (headOfMyArray < headOfYourArray) {
-    mergedArray[0] = headOfMyArray;
-  } else {
-    mergedArray[0] = headOfYourArray;
+  let currentIndexYours = 0;
+  let currentIndexMine = 0;
+  let currentIndexMerged = 0;
+
+  while (currentIndexMerged < myArray.length + yourArray.length) {
+    const firstUnmergedYours = yourArray[currentIndexYours];
+    const firstUnmergedMine = myArray[currentIndexMine];
+
+    if (firstUnmergedMine < firstUnmergedYours) {
+      mergedArray[currentIndexMerged] = firstUnmergedMine;
+      currentIndexMine++;
+    } else {
+      mergedArray[currentIndexMerged] = firstUnmergedYours;
+      currentIndexYours++;
+    }
+    currentIndexMerged++;
   }
+
+  // const headOfMyArray = myArray[0];
+  // const headOfYourArray = yourArray[0];
+
+  // if (headOfMyArray < headOfYourArray) {
+  //   mergedArray[0] = headOfMyArray;
+  // } else {
+  //   mergedArray[0] = headOfYourArray;
+  // }
 
   return mergedArray;
 }
