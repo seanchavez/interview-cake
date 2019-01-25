aa6d85b7deedc4af435afd267c5d8c7d85ca29a0
Sean Chavez <realseanchavez@gmail.com>
2019-01-24 16:28:09 -0800

    riffle recursive - optimized w/o slice

--- riffle-2.js
+++ riffle-2.js
@@ -1,12 +1,52 @@
-function isSingleRiffle(half1, half2, shuffledDeck) {
-  if (shuffledDeck.length === 0) {
+// function isSingleRiffle(half1, half2, shuffledDeck) {
+//   if (shuffledDeck.length === 0) {
+//     return true;
+//   }
+//   if (half1.length && half1[0] === shuffledDeck[0]) {
+//     return isSingleRiffle(half1.slice(1), half2, shuffledDeck.slice(1));
+//   } else if (half2.length && half2[0] === shuffledDeck[0]) {
+//     return isSingleRiffle(half1, half2.slice(1), shuffledDeck.slice(1));
+//   } else {
+//     return false;
+//   }
+// }
+
+function isSingleRiffle(
+  half1,
+  half2,
+  shuffledDeck,
+  shuffledDeckIndex,
+  half1Index,
+  half2Index,
+) {
+  shuffledDeckIndex =
+    typeof shuffledDeck !== 'undefined' ? shuffledDeckIndex : 0;
+  half1Index = typeof half1Index !== 'undefined' ? half1Index : 0;
+  half2Index = typeof half2Index !== 'undefined' ? half2Index : 0;
+
+  if (shuffledDeckIndex === shuffledDeck.length) {
     return true;
   }
-  if (half1.length && half1[0] === shuffledDeck[0]) {
-    return isSingleRiffle(half1.slice(1), half2, shuffledDeck.slice(1));
-  } else if (half2.length && half2[0] === shuffledDeck[0]) {
-    return isSingleRiffle(half1, half2.slice(1), shuffledDeck.slice(1));
+  if (
+    half1Index < half1Index.length &&
+    half1[half1Index] === shuffledDeck[shuffledDeckIndex]
+  ) {
+    half1Index++;
+  } else if (
+    half2Index < half2Index.length &&
+    half2[half2Index] === shuffledDeck[shuffledDeckIndex]
+  ) {
+    half2Index++;
   } else {
     return false;
   }
+  shuffleDeckIndex++;
+  return isSingleRiffle(
+    half1,
+    half2,
+    shuffledDeck,
+    shuffledDeckIndex,
+    half1Index,
+    half2Index,
+  );
 }
