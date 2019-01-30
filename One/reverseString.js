aa4aa37276f1d29fa0755e43ce2d55461ed46267
Sean Chavez <realseanchavez@gmail.com>
2019-01-30 10:30:10 -0800

    reverseString

---
+++ One/reverseString.js
@@ -0,0 +1,13 @@
+function reverse(arrayOfChars) {
+  let leftIndex = 0;
+  let rightIndex = arrayOfChars.length - 1;
+
+  while (leftIndex < rightIndex) {
+    const temp = arrayOfChars[leftIndex];
+    arrayOfChars[leftIndex] = arrayOfChars[rightIndex];
+    arrayOfChars[rightIndex] = temp;
+
+    leftIndex++;
+    rightIndex--;
+  }
+}
