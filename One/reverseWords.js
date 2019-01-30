721edb1afd56e2cda0b6a3205745d39e5a5d0899
Sean Chavez <realseanchavez@gmail.com>
2019-01-30 12:37:39 -0800

    reverseWords

--- One/reverseWords.js
+++ One/reverseWords.js
@@ -1,12 +1,20 @@
-function reverseCharacters(message) {
-  let leftIndex = 0;
-  let rightIndex = message.length - 1;
-
+function reverseCharacters(message, leftIndex, rightIndex) {
   while (leftIndex < rightIndex) {
     let temp = message[leftIndex];
     message[leftIndex] = message[rightIndex];
     message[rightIndex] = temp;
     leftIndex++;
     rightIndex--;
+  }
+}
+
+function reverseWords(message) {
+  reverseCharacters(message, 0, message.length - 1);
+  let currentWordStartIndex = 0;
+  for (let i = 0; i <= message.length; i++) {
+    if (i === message.length || message[i] === ' ') {
+      reverseCharacters(message, currentWordStartIndex, i - 1);
+      currentWordStartIndex = i + 1;
+    }
   }
 }
