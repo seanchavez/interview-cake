0bb82b07528aba39074846f2be803fbd20433dad
Sean Chavez <realseanchavez@gmail.com>
2019-01-30 22:21:36 -0800

    mergeArrays

--- One/mergeArrays.js
+++ One/mergeArrays.js
@@ -1,10 +1,33 @@
-function mergeSortedArrays(myArray, yourArray) {
-  const headOfMyArray = [0];
-  const headOfYourArray = [0];
-  if (headOfMyArray < headOfYourArray) {
-    mergedArray[0] = headOfMyArray;
-  } else {
-    mergedArray[0] = headOfYourArray;
+// function mergeSortedArrays(myArray, yourArray) {
+//   const headOfMyArray = [0];
+//   const headOfYourArray = [0];
+//   if (headOfMyArray < headOfYourArray) {
+//     mergedArray[0] = headOfMyArray;
+//   } else {
+//     mergedArray[0] = headOfYourArray;
+//   }
+//   return mergedArray;
+// }
+
+function mergeArrays(myArray, yourArray) {
+  const mergedArray = [];
+
+  let currentIndexYours = 0;
+  let currentIndexMine = 0;
+  let currentIndexMerged = 0;
+
+  while (currentIndexMerged < myArray.length + yourArray.length) {
+    const firstUnmergedYours = yourArray[currentIndexYours];
+    const firstUnmergedMine = myArray[currentIndexMine];
+
+    if (firstUnmergedMine < firstUnmergedYours) {
+      mergedArray[currentIndexMerged] = firstUnmergedMine;
+      currentIndexMine++;
+    } else {
+      mergedArray[currentIndexMerged] = firstUnmergedYours;
+      currentIndexYours++;
+    }
+    currentIndexMerged++;
   }
   return mergedArray;
 }
