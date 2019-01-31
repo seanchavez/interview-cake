80ff4b9f722a8c29fb91e21816507ef3a24faf53
Sean Chavez <realseanchavez@gmail.com>
2019-01-30 21:43:54 -0800

    find 0th index

---
+++ One/mergeArrays.js
@@ -0,0 +1,10 @@
+function mergeSortedArrays(myArray, yourArray) {
+  const headOfMyArray = [0];
+  const headOfYourArray = [0];
+  if (headOfMyArray < headOfYourArray) {
+    mergedArray[0] = headOfMyArray;
+  } else {
+    mergedArray[0] = headOfYourArray;
+  }
+  return mergedArray;
+}
