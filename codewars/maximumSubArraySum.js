d3073dffa7617f7500239a0263f60aada3d6ec84
Sean Chavez <realseanchavez@gmail.com>
2019-05-19 13:49:52 -0700

    edge cases, etc.

--- codewars/maximumSubArraySum.js
+++ codewars/maximumSubArraySum.js
@@ -1,14 +1,14 @@
-var maxSequence = function(arr){
-  let max = 0
+const maxSequence = function(arr){
+  let maxSum = 0
   
   for (let i = 1; i < arr.length; i++) {
     let sum = arr[i]
     for (j = i + 1; j < arr.length; j++) {
       sum += arr[j]
-      if (sum > max) max = sum
+      if (sum > maxSum) maxSum = sum
     } 
   }
-  return max
+  return (maxSum > 0 ? maxSum : 0)
 }
 
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
