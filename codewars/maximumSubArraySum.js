b9827c038c5f3e5b11101ef303fc197d1e7243a7
Sean Chavez <realseanchavez@gmail.com>
2019-05-20 19:09:53 -0700

    totally clueless

--- codewars/maximumSubArraySum.js
+++ codewars/maximumSubArraySum.js
@@ -1,37 +1,34 @@
-const maxSequence = function(arr){
-  let maxSum = 0
-  
+const maxSequence = function(arr) {
+  let maxSum = 0;
+
   for (let i = 1; i < arr.length; i++) {
-    let sum = arr[i]
+    let sum = arr[i];
     for (j = i + 1; j < arr.length; j++) {
-      sum += arr[j]
-      if (sum > maxSum) maxSum = sum
-    } 
+      sum += arr[j];
+      if (sum > maxSum) maxSum = sum;
+    }
   }
-  return (maxSum > 0 ? maxSum : 0)
-}
+  return maxSum > 0 ? maxSum : 0;
+};
 
 console.log(maxSequence([-2, 1, -3, 4, -1, 2, 1, -5, 4]));
 
-
-
-function wave(str){
-  const results = []
+function wave(str) {
+  const results = [];
   for (let i = 0; i < str.length; i++) {
-    const strArr = str.split('')
-    if (strArr[i] === ' ') continue
-    strArr[i] = strArr[i].toUpperCase()
-    results.push(strArr.join(''))
+    const strArr = str.split("");
+    if (strArr[i] === " ") continue;
+    strArr[i] = strArr[i].toUpperCase();
+    results.push(strArr.join(""));
   }
-  return results
+  return results;
 }
-
 
 // function diamond(n){
 //   if (n % 2 === 0 || n < 0) return null
 //   return makeTopOrBottomRow(n - 2).concat(makeMiddleRow(n), makeTopOrBottomRow(n - 2))
-//  } 
-  
+//  }
+
 //   function makeTopOrBottomRow(n) {
 //     const row = []
 //     for (i = 0; i < n + 2; i++) {
@@ -45,7 +42,7 @@ function wave(str){
 //     }
 //     return row.join('')
 //   }
-  
+
 //   function makeMiddleRow(n) {
 //     const row = []
 //     for (i = 0; i < n + 1; i++) {
@@ -59,25 +56,38 @@ function wave(str){
 //   }
 
 function diamond(n) {
-  if (n % 2 === 0 || n < 0) return null
-  let diamond = ''
-  let padding = (n - 1) / 2
-  for (i = 0; i < n; i + 2) {
-    diamond += diamond.padStart(2)
-    diamond += '*'.repeat(i)
-    //diamond += diamond.padEnd(padding)
-    padding--
-    
-    // for(j = 1; j < n; j++) {
-    //   // if (j === Math.floor(n / 2)) {
-    //   //   diamond += '*'
-    //   // }
-    // }
-    diamond += '\n'
+  if (n % 2 === 0 || n < 0) return null;
+  let diamond = "*";
+  let padding = (n - 1) / 2;
+  let count = 1;
+
+  while (count <= n) {
+    //console.log('fuck')
+    diamond =
+      diamond.repeat(count) +
+      diamond.padStart(padding) +
+      diamond.padEnd(padding) +
+      "\n";
+    count++;
+    // diamond += '\n'
   }
-  return diamond
+
+  //for (i = 0; i < n; i + 2) {
+  //   //diamond += diamond.padStart(padding)
+  //   diamond += diamond.repeat(i)
+  //   //diamond += diamond.padEnd(padding)
+  //   padding--
+
+  //   // for(j = 1; j < n; j++) {
+  //   //   // if (j === Math.floor(n / 2)) {
+  //   //   //   diamond += '*'
+  //   //   // }
+  //   // }
+  //   diamond += '\n'
+  // }
+  return diamond;
 }
 
-  console.log(diamond(3)) 
-  //console.log(makeTopOrBottomRow(1))
-  //console.log(makeMiddleRow(3))
+console.log(diamond(3));
+//console.log(makeTopOrBottomRow(1))
+//console.log(makeMiddleRow(3))
